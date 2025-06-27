979832637bd64e04e80b8af41f0d080346f9b8fb
moominfirdous@gmail.com <moominaik@gmail.com>
2025-06-27 21:10:45 +0530

    footer update 2025

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -22,7 +22,7 @@ const Footer = () => {
             <a href="/contactUs" className="relative text-black transition-colors duration-300 after:content-[''] after:absolute after:left-0 after:bottom-[-2px] after:w-full after:h-[1.5px] after:bg-black after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:duration-300 after:origin-left">Contact</a>
           </div>
           <p className="text-xs text-gray-700">
-            &copy; {new Date().getFullYear()} 2025 Forbes Logistix, LLC. All rights reserved.
+            &copy; {new Date().getFullYear()} Forbes Logistix, LLC. All rights reserved.
           </p>
         </div>
       </footer>
